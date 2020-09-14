37e18b666b227cc0b1d5a6ef27d2abd209352db1
phgrosjean <phgrosjean@sciviews.org>
2020-09-14 10:24:57 +0200

    Adaptation to {learndown} 1.1.0

--- setup.R
+++ setup.R
@@ -3,29 +3,84 @@ learndown <- list(
   baseurl = "https://wp.sciviews.org", # The base URL for the site
   shiny_imgdir = "images/shinyapps",   # The Shiny image directory (screenshots)
   svbox = 2020,                        # The SciViews Box version used
+  rstudio = "start_rstudio2020.html",  # Run Rstudio from the box
+  package = "BioDataScience3",         # Associated package for the exercices
   institutions = "UMONS",              # Known institutions
   courses = c(
-    "S-BIOG-025"                       # SDD3
+    "S-BIOG-025",                      # SDD3
+    "S-BIOG-043",                      # SDD4 (option)
+    "S-BIOG-077"                       # SDD5 (option)
+  ),
+  courses_names = c(
+    "Science des Données Biologiques III à l'UMONS",
+    "Science des Données Biologiques IV à l'UMONS",
+    "Science des Données Biologiques V à l'UMONS"
   )
 )
 
-h5p <- function(id, ...)
-  learndown::h5p(id, baseurl = learndown$baseurl, ...)
+## h5p(), launch_shiny(), learnr() & assignation() for exercises blocks, use:
+#
+# `r h5p(x, toc = "Short description")`
+#
+# `r launch_shiny(url, toc = "Short description")`
+#
+# `r learnr(id, title = "...", toc = "Short description")`
+#
+#```{r, echo=FALSE, results='asis'}
+#assignation("A01a_markdown", part = NULL,
+#  url = "https://github.com/BioDataScience-Course/sdd1_module01",
+#  # Note: one can include only one or two of these courses here!
+#  course.urls = c(
+#    'S-BIOG-025' = "https://classroom.github.com/a/...",
+#    'S-BIOG-043' = "https://classroom.github.com/a/...",
+#    'S-BIOG-077' = "https://classroom.github.com/a/..."),
+#  toc = "Assignation : réalisation d'un premier document en Markdown")
+#```
+#
+# Then, at the end of the module, create the exercises toc with:
+#
+# `r show_ex_toc()`
 
-launch_shiny <- function(url, fun = "BioDataScience::runApp",
+h5p <- function(id, toc = "", ...)
+  learndown::h5p(id, toc = toc, baseurl = learndown$baseurl,
+    toc.def = "Exercice H5P {id}",
+    h5p.img = "images/list-h5p.png",
+    h5p.link = paste(learndown$baseurl, "h5p", sep = "/"), ...)
+
+launch_shiny <- function(url, toc = "", fun = paste(learndown$package, "runApp", sep = "::"),
   #ENalt1 = "*Click to start the Shiny application*",
-  alt1 = "*Cliquez pour lancer l'application Shiny*",
-  #ENalt2 = paste0(
-  #EN  "*Click to start or run `{fun}(\"{app}\")` in the svbox",
-  #EN  learndown$svbox, ".*"), ...)
-  alt2 = paste0(
-    "*Cliquez pour lancer ou exécutez `{fun}(\"{app}\")` dans la svbox",
-    learndown$svbox, ".*"), ...)
-  learndown::launch_shiny(url = url, imgdir = learndown$shiny_imgdir, fun = fun,
-    alt1 = alt1, alt2 = alt2, ...)
+  alt1 = "*Cliquez pour lancer l'application Shiny.*",
+  #ENalt2 = "*Click to start or [run `{run.cmd}`]({run.url}{run.arg}) in RStudio.*",
+  alt2 = "*Cliquez pour lancer ou [exécutez dans RStudio]({run.url}{run.arg}) `{run.cmd}`.*", ...)
+  learndown::launch_shiny(url = url, toc = toc, imgdir = learndown$shiny_imgdir,
+    fun = fun, alt1 = alt1, alt2 = alt2, toc.def = "Application Shiny {app}",
+    run.url = paste(learndown$baseurl, "/", learndown$rstudio,  "?runrcode=", sep = ""),
+    app.img = "images/list-app.png",
+    app.link = paste(learndown$baseurl, "shiny_app", sep = "/"), ...)
 
-launch_learnr <- function(url, fun = "BioDataScience::run", ...)
-  launch_shiny(url = url, fun = fun, ...)
+# Note: not used yet!
+launch_learnr <- function(url, toc = "", fun = paste(learndown$package, "run", sep = "::"), ...)
+  launch_shiny(url = url, toc = toc, fun = fun, ...)
+
+learnr <- function(id, title = NULL, toc = "", package = learndown$package,
+  text = "Effectuez maintenant les exercices du tutoriel")
+  learndown::learnr(id = id, title = title, package = package, toc = toc,
+    text = text, toc.def = "Tutoriel {id}",
+    rstudio.url = paste(learndown$baseurl, learndown$rstudio, sep = "/"),
+    tuto.img = "images/list-tuto.png",
+    tuto.link = paste(learndown$baseurl, "tutorial", sep = "/"))
+
+# Note: use course.urls = c(`S-BIOG-025` = "classroom url1", `S-BIOG-943` = "classroom url2", `S-BIOG-077` = "classroom url3"), and url = link to Github template repository for the assignation
+assignation <- function(name, url, course.urls = NULL, part = NULL, toc = "",
+  texts = learndown::assignation_fr(course = "Assignation GitHub Classroom pour les \u00e9tudiants inscrits au cours de"))
+  learndown::assignation(name = name, url = url, course.urls = course.urls,
+    part = part, course.names = stats::setNames(learndown$courses_names,
+      learndown$courses),
+    toc = toc, texts = texts, assign.img = "images/list-assign.png",
+    assign.link = paste(learndown$baseurl, "github_assignation", sep = "/"))
+
+show_ex_toc <- function(header = "", clear.it = TRUE)
+  learndown::show_ex_toc(header = header, clear.it = clear.it)
 
 # Include javascript and css code for {learndown} additional features
 # in style.css and header.html, respectively
